78252708983ea004e753e2113ccf39131cbf1573
Justas Balcas <jbalcas@macos-justas.dyndns.cern.ch>
2018-02-19 14:30:20 +0100

    First test to write to opentsdb

--- src/python/t2Mon/compute/computeSAM.py
+++ src/python/t2Mon/compute/computeSAM.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
 """ """
+import time
+import socket
 from t2Mon.common.Utilities import externalCommand
 from t2Mon.common.configReader import ConfigReader
 from t2Mon.common.database.opentsdb import opentsdb
+
+CURRENT_TIME = int(time.time())
+HOST = socket.gethostname()
 
 def checkConfigForDB(config):
     if config.hasSection('opentsdb'):
@@ -18,7 +23,12 @@ def execute():
         if config.hasOption('compute', 'checks'):
             allChecks = config.getOption('compute', 'checks').split(',')
             print allChecks
-
+            # Execute all Checks, but before, we need to export few things
+            # And send all metrics. after we done, close metrics sender.
+            for check in allChecks:
+                key = check[:-3]  # Cut all not needed sh...
+                value = 3
+                dbBackend.sendMetric('compute.status.%s' % key, value, CURRENT_TIME, {'myhost': HOST, 'cmsCheck': key})
 
 if __name__ == "__main__":
     execute()
