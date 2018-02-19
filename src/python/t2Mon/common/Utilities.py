3017f082759d073ccf7236fc5c03eaa9144f914f
Justas Balcas <jbalcas@macos-justas.dyndns.cern.ch>
2018-02-19 14:05:56 +0100

    Removing psutil for now. This requires also gcc...

--- src/python/t2Mon/common/Utilities.py
+++ src/python/t2Mon/common/Utilities.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import subprocess
 import shlex
-import psutil
+#import psutil
 
 
 def externalCommand(command):
@@ -37,34 +37,34 @@ def getItemPos(item, inList):
             return i
     return -1
 
-
-def getProcInfo(procPID):
-    procS = psutil.Process(int(procPID))
-    itemOut = {}
-    itemOut['CreateTime'] = procS.create_time()
-    ioCounters = procS.io_counters()
-    itemOut['ReadCount'] = ioCounters.read_count
-    itemOut['WriteCount'] = ioCounters.write_count
-    itemOut['ReadBytes'] = ioCounters.read_bytes
-    itemOut['WriteBytes'] = ioCounters.write_bytes
-    itemOut['ReadChars'] = ioCounters.read_chars
-    itemOut['WriteChars'] = ioCounters.write_chars
-    itemOut['NumFds'] = procS.num_fds()
-    memInfo = procS.memory_full_info()
-    memUsageProc = itemOut.setdefault('MemoryUsage', {})
-    memUsageProc['Rss'] = memInfo.rss
-    memUsageProc['Vms'] = memInfo.vms
-    memUsageProc['Shared'] = memInfo.shared
-    memUsageProc['Text'] = memInfo.text
-    memUsageProc['Lib'] = memInfo.lib
-    memUsageProc['Data'] = memInfo.data
-    memUsageProc['Dirty'] = memInfo.dirty
-    memUsageProc['Uss'] = memInfo.uss
-    memUsageProc['Pss'] = memInfo.pss
-    memUsageProc['Swap'] = memInfo.swap
-    itemOut['Connections'] = {}
-    for item in procS.connections():
-        if item.status not in itemOut['Connections'].keys():
-            itemOut['Connections'][item.status] = 0
-        itemOut['Connections'][item.status] += 1
-    return itemOut
+# 
+# def getProcInfo(procPID):
+#     procS = psutil.Process(int(procPID))
+#     itemOut = {}
+#     itemOut['CreateTime'] = procS.create_time()
+#     ioCounters = procS.io_counters()
+#     itemOut['ReadCount'] = ioCounters.read_count
+#     itemOut['WriteCount'] = ioCounters.write_count
+#     itemOut['ReadBytes'] = ioCounters.read_bytes
+#     itemOut['WriteBytes'] = ioCounters.write_bytes
+#     itemOut['ReadChars'] = ioCounters.read_chars
+#     itemOut['WriteChars'] = ioCounters.write_chars
+#     itemOut['NumFds'] = procS.num_fds()
+#     memInfo = procS.memory_full_info()
+#     memUsageProc = itemOut.setdefault('MemoryUsage', {})
+#     memUsageProc['Rss'] = memInfo.rss
+#     memUsageProc['Vms'] = memInfo.vms
+#     memUsageProc['Shared'] = memInfo.shared
+#     memUsageProc['Text'] = memInfo.text
+#     memUsageProc['Lib'] = memInfo.lib
+#     memUsageProc['Data'] = memInfo.data
+#     memUsageProc['Dirty'] = memInfo.dirty
+#     memUsageProc['Uss'] = memInfo.uss
+#     memUsageProc['Pss'] = memInfo.pss
+#     memUsageProc['Swap'] = memInfo.swap
+#     itemOut['Connections'] = {}
+#     for item in procS.connections():
+#         if item.status not in itemOut['Connections'].keys():
+#             itemOut['Connections'][item.status] = 0
+#         itemOut['Connections'][item.status] += 1
+#     return itemOut
