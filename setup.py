0042c889b40c19b5fcfd9dbef04256730a2c41e9
Justas Balcas <jbalcas@macos-justas.dyndns.cern.ch>
2018-02-19 14:02:34 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from distutils.core import setup
+from setupUtilities import list_packages, get_py_modules
 
 setup(name='t2Mon',
       version='0.1',
@@ -7,7 +8,8 @@ setup(name='t2Mon',
       author='Justas Balcas',
       author_email='juztas@gmail.com',
       url='https://github.com/jbalcas/T2_SCRIPTS',
-      packages=['t2Mon'],
+      packages=['t2Mon'] + list_packages(['src/python/t2Mon/']),
+      py_modules=get_py_modules(['src/python/t2Mon']),
       install_requires=['potsdb', 'psutil'],
       package_dir={'': 'src/python/'},
       data_files=[('/etc/', ['packaging/t2_maitenance.conf'])],
